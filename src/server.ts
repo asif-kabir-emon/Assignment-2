24c97e8c25568c4a5552592a9b2c951452bd19f2
Asif Kabir Emon <asif.kabir@coolriots.io>
2023-11-24 21:13:04 +0800

    Vercel config added

--- src/server.ts
+++ src/server.ts
@@ -3,12 +3,14 @@ import app from './app';
 import mongoose from 'mongoose';
 import config from './app/config';
 
+const PORT = process.env.PORT || 4000;
+
 async function main() {
     try {
         await mongoose.connect(config.databaseURL as string);
 
-        app.listen(config.port, () => {
-            console.log(`Server is running on port ${config.port}`);
+        app.listen(PORT, () => {
+            console.log(`Server is running on port ${PORT}`);
         });
     } catch (error) {
         console.log(error);
